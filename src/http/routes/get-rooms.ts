4d3ca2e6b6d2f24037a4450293375434d497ee80
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-07-08 18:46:24 -0300

    feat: enhance getRooms route to include questions count and created date

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,4 +1,5 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { count, eq } from 'drizzle-orm'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
 
@@ -8,8 +9,12 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        questionsCount: count(schema.questions.id),
+        createdAt: schema.rooms.createdAt,
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.rooms.id, schema.questions.roomId))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt)
 
     return results
