88512cc6b0a1ae90d5893350095cf6a4d96dd7c3
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-07-09 22:38:30 -0300

    fix: remove environment variables response from API handler

--- api/index.js
+++ api/index.js
@@ -46,7 +46,6 @@ async function getApp() {
 
 export default async (req, res) => {
   try {
-    return res.status(200).json({ envs: process.env })
     const app = await getApp()
     const response = await app.inject({
       method: req.method || 'GET',
